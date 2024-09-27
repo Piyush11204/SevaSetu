52ebb97d974e10e61c17e986afd9f9bcb577d6d5
Piyush11204 <138059020+Piyush11204@users.noreply.github.com>
2024-09-28 04:43:32 +0530

    addlocation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  https://cdn.botpress.cloud/webchat/v2/shareable.html?botId=a4d28e1b-8f74-4721-928a-aca397751e70
   return (
     <>
       <h1>Hello</h1>
